80e8a7a0f1a283cef9c38e4c650acf62d97ccba7
Ren <sergeysolovyov2016@gmail.com>
2023-12-10 01:44:39 +0200

    * refactor and cleanup code * added missed method impl

--- include/board_utils.h
+++ include/board_utils.h
@@ -6,8 +6,8 @@
 
 class BoardUtils {
   public:
-    static const int NUMBER_SQUARES{64};
-    static const int NUMBER_SQUARE_PER_ROW{8};
+    static constexpr int NUMBER_SQUARES{64};
+    static constexpr int NUMBER_SQUARE_PER_ROW{8};
     static const std::array<bool, NUMBER_SQUARES> EIGHTH_RANK;
     static const std::array<bool, NUMBER_SQUARES> SEVENTH_RANK;
     static const std::array<bool, NUMBER_SQUARES> SIXTH_RANK;
@@ -20,17 +20,15 @@ class BoardUtils {
     static const std::array<bool, NUMBER_SQUARES> SECOND_COLUMN;
     static const std::array<bool, NUMBER_SQUARES> SEVENTH_COLUMN;
     static const std::array<bool, NUMBER_SQUARES> EIGHT_COLUMN;
-    static int getCoordinateAtPosition(std::string position);
+    static int getCoordinateAtPosition(const std::string& position);
     static std::string getPositionAtCoordinate(int coordinate);
     static bool isValidSquareCoordinate(int coordinate);
 
   private:
-    static const std::array<std::string, BoardUtils::NUMBER_SQUARES>
-        ALGEBRAIC_NOTATION;
+    static const std::array<std::string, NUMBER_SQUARES> ALGEBRAIC_NOTATION;
     static const std::map<std::string, int> POSITION_TO_COORDINATE;
-    static std::array<bool, BoardUtils::NUMBER_SQUARES> initRow(int rowNumber);
-    static std::array<bool, BoardUtils::NUMBER_SQUARES>
-    initColumn(int colNumber);
+    static std::array<bool, NUMBER_SQUARES> initRow(int rowNumber);
+    static std::array<bool, NUMBER_SQUARES> initColumn(int colNumber);
     static std::map<std::string, int> initializePositionToCoordinate();
 };
 
--- src/engine/board_utils.cpp
+++ src/engine/board_utils.cpp
@@ -1,29 +1,29 @@
 #include "board_utils.h"
 
 const std::array<bool, BoardUtils::NUMBER_SQUARES> BoardUtils::EIGHTH_RANK =
-    BoardUtils::initRow(0);
+    initRow(0);
 const std::array<bool, BoardUtils::NUMBER_SQUARES> BoardUtils::SEVENTH_RANK =
-    BoardUtils::initRow(8);
+    initRow(8);
 const std::array<bool, BoardUtils::NUMBER_SQUARES> BoardUtils::SIXTH_RANK =
-    BoardUtils::initRow(16);
+    initRow(16);
 const std::array<bool, BoardUtils::NUMBER_SQUARES> BoardUtils::FIFTH_RANK =
-    BoardUtils::initRow(24);
+    initRow(24);
 const std::array<bool, BoardUtils::NUMBER_SQUARES> BoardUtils::FOURTH_RANK =
-    BoardUtils::initRow(32);
+    initRow(32);
 const std::array<bool, BoardUtils::NUMBER_SQUARES> BoardUtils::THIRD_RANK =
-    BoardUtils::initRow(40);
+    initRow(40);
 const std::array<bool, BoardUtils::NUMBER_SQUARES> BoardUtils::SECOND_RANK =
-    BoardUtils::initRow(48);
+    initRow(48);
 const std::array<bool, BoardUtils::NUMBER_SQUARES> BoardUtils::FIRST_RANK =
-    BoardUtils::initRow(56);
+    initRow(56);
 const std::array<bool, BoardUtils::NUMBER_SQUARES> BoardUtils::FIRST_COLUMN =
-    BoardUtils::initRow(0);
+    initRow(0);
 const std::array<bool, BoardUtils::NUMBER_SQUARES> BoardUtils::SECOND_COLUMN =
-    BoardUtils::initColumn(1);
+    initColumn(1);
 const std::array<bool, BoardUtils::NUMBER_SQUARES> BoardUtils::SEVENTH_COLUMN =
-    BoardUtils::initColumn(6);
+    initColumn(6);
 const std::array<bool, BoardUtils::NUMBER_SQUARES> BoardUtils::EIGHT_COLUMN =
-    BoardUtils::initColumn(7);
+    initColumn(7);
 const std::array<std::string, BoardUtils::NUMBER_SQUARES>
     BoardUtils::ALGEBRAIC_NOTATION = {
         "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8", "a7", "b7", "c7",
@@ -33,34 +33,43 @@ const std::array<std::string, BoardUtils::NUMBER_SQUARES>
         "e3", "f3", "g3", "h3", "a2", "b2", "c2", "d2", "e2", "f2", "g2",
         "h2", "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"};
 const std::map<std::string, int> BoardUtils::POSITION_TO_COORDINATE =
-    BoardUtils::initializePositionToCoordinate();
+    initializePositionToCoordinate();
 
-int BoardUtils::getCoordinateAtPosition(std::string position) {
-    return BoardUtils::POSITION_TO_COORDINATE.at(position);
+int BoardUtils::getCoordinateAtPosition(const std::string &position) {
+    return POSITION_TO_COORDINATE.at(position);
 }
 
 std::string BoardUtils::getPositionAtCoordinate(int coordinate) {
-    return BoardUtils::ALGEBRAIC_NOTATION.at(coordinate);
+    return ALGEBRAIC_NOTATION.at(coordinate);
 }
 
 bool BoardUtils::isValidSquareCoordinate(int coordinate) {
     return coordinate >= 0 && coordinate < NUMBER_SQUARES;
 }
 
-std::array<bool, BoardUtils::NUMBER_SQUARES>
-BoardUtils::initRow(int rowNumber) {
-    return std::array<bool, BoardUtils::NUMBER_SQUARES>();
+std::array<bool, 64> BoardUtils::initRow(int rowNumber) {
+    std::array<bool, NUMBER_SQUARES> row{};
+    do {
+        row[rowNumber] = true;
+        rowNumber++;
+    } while (rowNumber % NUMBER_SQUARE_PER_ROW);
+    return row;
 }
 
 std::array<bool, BoardUtils::NUMBER_SQUARES>
 BoardUtils::initColumn(int colNumber) {
-    return std::array<bool, BoardUtils::NUMBER_SQUARES>();
+    std::array<bool, NUMBER_SQUARES> column{};
+    do {
+        column[colNumber] = true;
+        colNumber += NUMBER_SQUARE_PER_ROW;
+    } while (colNumber < NUMBER_SQUARES);
+    return column;
 }
 
 std::map<std::string, int> BoardUtils::initializePositionToCoordinate() {
     std::map<std::string, int> positionToCoordinate;
-    for (int i{0}; i < BoardUtils::NUMBER_SQUARES; i++) {
-        positionToCoordinate[BoardUtils::ALGEBRAIC_NOTATION[i]] = i;
+    for (int i{0}; i < NUMBER_SQUARES; i++) {
+        positionToCoordinate[ALGEBRAIC_NOTATION[i]] = i;
     }
     return positionToCoordinate;
 }
