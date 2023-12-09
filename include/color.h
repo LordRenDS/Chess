790e129d800774a976a743150519be8cabc83822
Ren <sergeysolovyov2016@gmail.com>
2023-12-10 01:48:56 +0200

    * added method to Color

--- include/color.h
+++ include/color.h
@@ -1,11 +1,8 @@
 #ifndef COLOR_H
 #define COLOR_H
-#include <string>
 
-// enum class Color {
-//     BLACK,
-//     WHITE
-// };
+#include "board_utils.h"
+#include <string>
 
 class Color {
   public:
@@ -21,6 +18,30 @@ class Color {
                 return "w";
         }
     };
+    static int getDirection(ColorT color) {
+        switch (color) {
+            case ColorT::BLACK:
+                return 1;
+            case ColorT::WHITE:
+                return -1;
+        }
+    };
+    static int getOppositeDirection(ColorT color) {
+        switch (color) {
+            case ColorT::BLACK:
+                return -1;
+            case ColorT::WHITE:
+                return 1;
+        }
+    }
+    static bool isPawnPromotionSquare(ColorT color, int position) {
+        switch (color) {
+            case ColorT::BLACK:
+                return BoardUtils::FIFTH_RANK[position];
+            case ColorT::WHITE:
+                return BoardUtils::EIGHTH_RANK[position];
+        }
+    }
 };
 
 #endif
