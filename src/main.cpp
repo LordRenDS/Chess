a818e70c9cab0499413030c66a13d6696a428e99
Ren <sergeysolovyov2016@gmail.com>
2023-12-18 01:07:05 +0200

    * added main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
-
+#include "board.h"
+#include "cli.h"
+#include "figure.h"
+#include "move.h"
+#include "player.h"
+#include <iostream>
 
 int main() {
+    auto table(std::make_unique<Table>());
+    table->startGame();
+    std::cin.get();
     return 0;
 }
