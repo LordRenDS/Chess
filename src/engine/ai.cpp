23251dfab14aad362a872c509e5f381ed3f44fe0
Ren <sergeysolovyov2016@gmail.com>
2023-12-20 00:30:01 +0200

    bugfix

--- src/engine/ai.cpp
+++ src/engine/ai.cpp
@@ -7,37 +7,46 @@
 #include <memory>
 
 Move *minimaxRoot(int depth, Table &table, bool isMax) {
-    int bestScore{};
+    int bestScore{1000};
     Move *bestMove{};
-    auto calculationTable(std::make_unique<Table>(table));
-    for (auto &move : calculationTable->getBlackPlayer()->getLegalMoves()) {
+    for (auto &move : table.getBlackPlayer()->getLegalMoves()) {
+        auto calculationTable(std::make_unique<Table>(table));
+        Move *mirrorMove{};
+        for (auto &calculationMove :
+             calculationTable->getBlackPlayer()->getLegalMoves()) {
+            if (calculationMove->equals(*move))
+                mirrorMove = calculationMove.get();
+        }
         if (calculationTable->getBlackPlayer()->makeMove(
-                move.get(), *calculationTable->getBoard(),
+                mirrorMove, *calculationTable->getBoard(),
                 calculationTable->getWhitePlayer()) ==
             MoveStatus::LEAVE_PLAYER_IN_CHEK)
             continue;
-        Score score = minimax(depth - 1, *calculationTable, 1000, 1000, !isMax);
+        Score score = minimax(depth - 1, *calculationTable, 10000, 10000, !isMax);
         if (score.white < bestScore) {
             bestScore = score.white;
             bestMove = move.get();
         }
     }
-    for (auto &move : table.getBlackPlayer()->getLegalMoves()) {
-        if (*bestMove == *move.get())
-            return move.get();
-    }
+    return bestMove;
 }
 
 Score minimax(int depth, Table &table, int alpha, int beta, bool isMax) {
     if (!depth)
         return Score{table.getBoard()->evaluateBoard(Color::ColorT::WHITE),
                      table.getBoard()->evaluateBoard(Color::ColorT::BLACK)};
-    auto calculationTable(std::make_unique<Table>(table));
     Score score{};
     Score nextScore{};
     if (isMax) {
-        score = {100, -100};
-        for (auto &move : calculationTable->getBlackPlayer()->getLegalMoves()) {
+        score = {1000, -1000};
+        for (auto &move : table.getBlackPlayer()->getLegalMoves()) {
+            auto calculationTable(std::make_unique<Table>(table));
+            Move *mirrorMove{};
+            for (auto &calculationMove :
+                 calculationTable->getBlackPlayer()->getLegalMoves()) {
+                if (calculationMove->equals(*move))
+                    mirrorMove = calculationMove.get();
+            }
             if (calculationTable->getBlackPlayer()->makeMove(
                     move.get(), *calculationTable->getBoard(),
                     calculationTable->getWhitePlayer()) ==
@@ -46,14 +55,21 @@ Score minimax(int depth, Table &table, int alpha, int beta, bool isMax) {
             nextScore =
                 minimax(depth - 1, *calculationTable, alpha, beta, !isMax);
             score = score.white < nextScore.white ? score : nextScore;
-            alpha = std::min(alpha, score.white);
-            if (beta >= alpha)
-                return score;
+//            alpha = std::min(alpha, score.white);
+//            if (beta >= alpha)
+//                return score;
         }
         return score;
     } else {
-        score = {-100, 100};
-        for (auto &move : calculationTable->getWhitePlayer()->getLegalMoves()) {
+        score = {-1000, 1000};
+        for (auto &move : table.getWhitePlayer()->getLegalMoves()) {
+            auto calculationTable(std::make_unique<Table>(table));
+            Move *mirrorMove{};
+            for (auto &calculationMove :
+                 calculationTable->getWhitePlayer()->getLegalMoves()) {
+                if (calculationMove->equals(*move))
+                    mirrorMove = calculationMove.get();
+            }
             if (calculationTable->getWhitePlayer()->makeMove(
                     move.get(), *calculationTable->getBoard(),
                     calculationTable->getBlackPlayer()) ==
@@ -62,9 +78,9 @@ Score minimax(int depth, Table &table, int alpha, int beta, bool isMax) {
             nextScore =
                 minimax(depth - 1, *calculationTable, alpha, beta, !isMax);
             score = score.black < nextScore.black ? score : nextScore;
-            beta = std::min(beta, score.black);
-            if (beta >= alpha)
-                return score;
+//            beta = std::min(beta, score.black);
+//            if (beta >= alpha)
+//                return score;
         }
         return score;
     }
